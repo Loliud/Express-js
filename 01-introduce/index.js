73e2c2876eb429edeeb5d5ba2a05e56e84cbb714
Loliud <anhhaiquynhon123@gmail.com>
2019-08-30 22:28:47 +0700

    demo express

---
+++ 01-introduce/index.js
@@ -0,0 +1,13 @@
+const app = require('express')();
+const port = 3000;
+
+
+app.get('/', (req, res) =>{
+    res.send('<h1>Xin chao cac ban </h1>');
+});
+
+
+
+app.listen(port, () =>{
+    console.log('Server listening on port: ' + port);
+});
