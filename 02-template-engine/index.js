1c7a1d6345333b4053b82ac8fe04df1466728ea1
Loliud <anhhaiquynhon123@gmail.com>
2019-09-02 11:00:09 +0700

    query parameters

--- 02-template-engine/index.js
+++ 02-template-engine/index.js
@@ -3,6 +3,13 @@ const port = 3000;
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+const users = [
+    {name: 'Ky Anh'},
+    {name: 'Ngoc Minh'},
+    {name: 'Minh Tan'},
+    {name: 'Hoang Son'}
+]
+
 
 app.get('/', (req, res) =>{
     res.render('index.pug');
@@ -10,14 +17,19 @@ app.get('/', (req, res) =>{
 
 app.get('/users', (req, res) =>{
     res.render('users/index.pug', {
-        users: [
-            {name: 'Ky Anh'},
-            {name: 'Ngoc Minh'},
-            {name: 'Minh Tan'},
-            {name: 'Hoang Son'}
-        ]
+        users: users
     });
 });
+app.get('/users/search', (req, res) =>{
+    const {q} = req.query;
+    const searchUsers = users.filter(user =>{
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+    res.render('users/index.pug', {
+        users: searchUsers,
+        q: q
+    });
+})
 
 
 
